548a5858c760a3eb2219c5ea6f02dd2e5ce4ee62
mohammadjahandide <67603446+mohammadjahandide@users.noreply.github.com>
2020-07-25 19:21:29 +0430

    Update LoginPageController.java

--- ChildReddit/src/com/view/controller/LoginPageController.java
+++ ChildReddit/src/com/view/controller/LoginPageController.java
@@ -29,6 +29,16 @@ public class LoginPageController {
     @FXML
     private PasswordField passwordField;
 
+
+    @FXML
+    private TextField passwordShowField;
+
+    @FXML
+    private ImageView showPassword;
+
+    @FXML
+    private ImageView hidePassword;
+
     @FXML
     private JFXButton loginButton;
 
@@ -39,6 +49,11 @@ public class LoginPageController {
     private Label incorrectLabel;
 
     public void initialize(){
+        showPassword.setVisible(false);
+        passwordShowField.setVisible(false);
+        hidePassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> showPassword());
+        showPassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> hidePassword());
+
         loginButton.setOnAction(event -> {login();});
 
         signupButton.setOnAction(event -> {signup();});
@@ -56,10 +71,31 @@ public class LoginPageController {
                 "Reddit - Search '" + s + "'");
     }
 
+    private void showPassword(){
+        hidePassword.setVisible(false);
+        passwordShowField.setText(passwordField.getText());
+        passwordShowField.setVisible(true);
+        showPassword.setVisible(true);
+        passwordField.setVisible(false);
+    }
+
+    private void hidePassword(){
+        showPassword.setVisible(false);
+        hidePassword.setVisible(true);
+        passwordField.setText(passwordShowField.getText());
+        passwordField.setVisible(true);
+        passwordShowField.setVisible(false);
+    }
+
     private void login(){
         incorrectLabel.setText("");
         String username = usernameField.getText().replaceFirst("\\s++$", "");
-        String password = passwordField.getText().replaceFirst("\\s++$", "");
+        String password;
+        if(hidePassword.isVisible()){
+            password = passwordField.getText().replaceFirst("\\s++$", "");
+        }else{
+            password = passwordShowField.getText().replaceFirst("\\s++$", "");
+        }
 
         User user = User.login(username, password);
         if(user == null){
@@ -74,17 +110,17 @@ public class LoginPageController {
     private void signup(){
         signupButton.getScene().getWindow().hide();
 
-        OpenWindow.openWindow("../SignupPage.fxml",new SignupPageController(), "Reddit - Signup Page");
+        OpenWindow.openWindow("../SignupPage.fxml",new SignupPageController(), "ChildReddit - Signup Page");
     }
 
     private void homePage(){
         loginButton.getScene().getWindow().hide();
-        OpenWindow.openWindow("../HomePage.fxml",new HomePageController(null), "Reddit - HomePage");
+        OpenWindow.openWindow("../HomePage.fxml",new HomePageController(null), "ChildReddit - HomePage");
     }
 
     private void profilePage(User user){
         loginButton.getScene().getWindow().hide();
-        OpenWindow.openWindow("../ProfilePage.fxml",new ProfilePageController(user, user), "Reddit - Profile Page " + user.getUserName());
+        OpenWindow.openWindow("../ProfilePage.fxml",new ProfilePageController(user, user), "ChildReddit - Profile Page " + user.getUserName());
     }
 
 }
