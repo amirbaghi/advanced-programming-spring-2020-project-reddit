3ddf1a33e647f2efecaf9b1fd83b9c506d12c808
mohammadjahandide <67603446+mohammadjahandide@users.noreply.github.com>
2020-07-25 19:26:19 +0430

    Update ShortLoginController.java

--- ChildReddit/src/com/view/controller/ShortLoginController.java
+++ ChildReddit/src/com/view/controller/ShortLoginController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class ShortLoginController {
     private User user;
@@ -20,6 +22,16 @@ public class ShortLoginController {
     private PasswordField passwordField;
 
     @FXML
+    private TextField passwordShowField;
+
+    @FXML
+    private ImageView showPassword;
+
+    @FXML
+    private ImageView hidePassword;
+
+
+    @FXML
     private Label incorrectLabel;
 
     @FXML
@@ -27,16 +39,41 @@ public class ShortLoginController {
 
     @FXML
     public void initialize() {
+
+        showPassword.setVisible(false);
+        passwordShowField.setVisible(false);
+        hidePassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> showPassword());
+        showPassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> hidePassword());
         incorrectLabel.setText("");
 
         loginButton.setOnAction(event -> login());
     }
 
+    private void showPassword(){
+        hidePassword.setVisible(false);
+        passwordShowField.setText(passwordField.getText());
+        passwordShowField.setVisible(true);
+        showPassword.setVisible(true);
+        passwordField.setVisible(false);
+    }
+
+    private void hidePassword() {
+        showPassword.setVisible(false);
+        hidePassword.setVisible(true);
+        passwordField.setText(passwordShowField.getText());
+        passwordField.setVisible(true);
+        passwordShowField.setVisible(false);
+    }
+
     private void login(){
         incorrectLabel.setText("");
         String username = usernameField.getText().replaceFirst("\\s++$", "");
-        String password = passwordField.getText().replaceFirst("\\s++$", "");
-
+        String password;
+        if(hidePassword.isVisible()){
+            password = passwordField.getText().replaceFirst("\\s++$", "");
+        }else{
+            password = passwordShowField.getText().replaceFirst("\\s++$", "");
+        }
         User user = User.login(username, password);
         if(user == null){
             incorrectLabel.setText("Incorrect username or password");
