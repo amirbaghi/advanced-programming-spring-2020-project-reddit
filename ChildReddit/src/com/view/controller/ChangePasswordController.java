e573bf6c2030ed1b981e370acaf613a6f1231ea7
mohammadjahandide <67603446+mohammadjahandide@users.noreply.github.com>
2020-07-25 19:18:48 +0430

    Update ChangePasswordController.java

--- ChildReddit/src/com/view/controller/ChangePasswordController.java
+++ ChildReddit/src/com/view/controller/ChangePasswordController.java
@@ -6,6 +6,9 @@ import com.model.Validation;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class ChangePasswordController {
     private User user;
@@ -28,8 +31,39 @@ public class ChangePasswordController {
 
     @FXML
     private Label incorrectLabel;
+
+    @FXML
+    private TextField oldPasswordShowField;
+
+    @FXML
+    private TextField newPasswordShowField;
+
+    @FXML
+    private ImageView showOldPassword;
+
+    @FXML
+    private ImageView showNewPassword;
+
+    @FXML
+    private ImageView hideOldPassword;
+
+    @FXML
+    private ImageView hideNewPassword;
+
+
     @FXML
     void initialize() {
+
+        oldPasswordShowField.setVisible(false);
+        showOldPassword.setVisible(false);
+        newPasswordShowField.setVisible(false);
+        showNewPassword.setVisible(false);
+
+        hideOldPassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> showOLdPassword());
+        hideNewPassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> showNewPassword());
+
+        showOldPassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> hideOldPassword());
+        showNewPassword.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> hideNewPassword());
 
         saveButton.setOnAction(event -> {changePassword();});
 
@@ -37,9 +71,53 @@ public class ChangePasswordController {
 
     }
 
+    private void showOLdPassword(){
+        hideOldPassword.setVisible(false);
+        oldPasswordShowField.setText(oldPasswordField.getText());
+        oldPasswordShowField.setVisible(true);
+        showOldPassword.setVisible(true);
+        oldPasswordField.setVisible(false);
+    }
+
+    private void showNewPassword() {
+        hideNewPassword.setVisible(false);
+        showNewPassword.setVisible(true);
+        newPasswordShowField.setText(newPasswordField.getText());
+        newPasswordShowField.setVisible(true);
+        newPasswordField.setVisible(false);
+    }
+
+    private void hideOldPassword(){
+        showOldPassword.setVisible(false);
+        oldPasswordField.setText(oldPasswordShowField.getText());
+        oldPasswordField.setVisible(true);
+        hideOldPassword.setVisible(true);
+        oldPasswordShowField.setVisible(false);
+    }
+
+    private void hideNewPassword() {
+        showNewPassword.setVisible(false);
+        hideNewPassword.setVisible(true);
+        newPasswordField.setText(newPasswordShowField.getText());
+        newPasswordField.setVisible(true);
+        newPasswordShowField.setVisible(false);
+    }
+
     private void changePassword(){
-        String oldPassword = oldPasswordField.getText().replaceFirst("\\s++$", "");
-        String newPassword = newPasswordField.getText().replaceFirst("\\s++$", "");
+        incorrectLabel.setText("");
+        String oldPassword;
+        if(hideOldPassword.isVisible()){
+            oldPassword = oldPasswordField.getText().replaceFirst("\\s++$", "");;
+        }else{
+            oldPassword = oldPasswordShowField.getText().replaceFirst("\\s++$", "");
+        }
+
+        String newPassword;
+        if(hideNewPassword.isVisible()){
+            newPassword = newPasswordField.getText().replaceFirst("\\s++$", "");
+        }else{
+            newPassword = newPasswordShowField.getText().replaceFirst("\\s++$", "");
+        }
 
         int valid = Validation.passwordValidation(newPassword);
         if(valid == 0){
